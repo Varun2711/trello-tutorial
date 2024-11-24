bdc6f70bfea966919f69be680a75dd055d4474ed
Varun Patel <rvjpatel99@gmail.com>
2024-11-24 13:57:50 -0800

    finished marketing page and started auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+const Platformlayout = ({
+    children 
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
